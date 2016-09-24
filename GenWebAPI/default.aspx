a70d6d3d0d202b01ac9b05f6b38f7754f38f8b1a
Jeff Jones <spjeff@spjeff.com>
2016-09-23 22:24:17 -0500

    Code
    * initial commit

---
+++ GenWebAPI/default.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="GenWebAPI._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Generate WebAPI ZIP</title>
+</head>
+<style type="text/css">
+    .main {
+        margin-left: 30px;
+        margin-right: 30px;
+        margin-bottom: 15px;
+        padding: 15px;
+
+        background-color: #ebebeb;
+    }
+    .download {
+        background-color:deepskyblue;
+        padding:10px;
+    }
+</style>
+<body>
+    <form id="form1" runat="server">
+        <h1>Generate WebAPI ZIP&nbsp; <img src="API-icon.png" height="50" /></h1>
+        <div class="main">
+            <p>Type a new project name and click download ZIP.</p>
+            <p>Template project will have all internal references renamed  (CSPROJ, CS, assembly properties, namespace, etc.).   After project opens press F5 to run and view homepage in the browser.  Clicking on "api/Hello" will execute the default controller and return JSON/XML response HTTP 200 with the current time.  Enjoy!</p>
+            <br />
+            <p>Much faster than making a new WebAPI project, configuring CORS, configuring Windows Auth, creating readme.aspx showing build number.  More than a base template this has real world WebAPI configurations ready to go.</p>
+            
+            <hr />
+            <br />
+            <ol>
+                <asp:Label ID="Label1" runat="server" Text="Web API Name"></asp:Label>
+                <li>
+                    <asp:TextBox ID="ProjectName" runat="server"></asp:TextBox></li>
+                <br />
+                <li>
+                    <asp:Button CssClass="download" ID="Generate" runat="server" Text="Download Visual Studio ZIP" OnClick="Generate_Click" />
+                </li>
+                <br />
+                <li>Press F5 to run and test</li>
+            </ol>
+            <br />
+            <hr />
+            <img src="tree.gif" /><br />
+            <img src="1.gif" /><br />
+            <img src="2.gif" />
+            <br />
+
+
+        </div>
+        
+        <div>
+            <img src="https://en.gravatar.com/userimage/46254763/1789248e2a473e8fbfa80818faf70551.jpeg" height="40" width="40" />&nbsp;
+            <a href="https://twitter.com/spjeff">@spjeff</a>&nbsp;|&nbsp;
+            <a href="http://www.spjeff.com/">spjeff.com</a>
+        </div>
+
+    </form>
+
+    <script type="text/javascript">
+        window.onload = function () {
+            document.getElementById('<%= ProjectName.ClientID%>').focus();
+        };
+    </script>
+</body>
+</html>
---
+++ GenWebAPI/default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+using System.IO.Compression;
+
+namespace GenWebAPI
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Generate_Click(object sender, EventArgs e)
+        {
+            string WebAPIName = ProjectName.Text;
+
+            Guid assemblyGuid = Guid.NewGuid();
+            string baseFolder = Server.MapPath("~/") + "ZIPTEMP\\" + assemblyGuid + "\\";
+            if (Directory.Exists(baseFolder))
+            {
+                Directory.Delete(baseFolder, true);
+            }
+            Directory.CreateDirectory(baseFolder);
+            ZipFile.ExtractToDirectory(Server.MapPath("~/") + "HelloTime.zip", baseFolder);
+
+            //base download folder
+            string baseDownload = HttpContext.Current.Server.MapPath("~/") + "download" + "\\";
+            if (!Directory.Exists(baseDownload))
+            {
+                Directory.CreateDirectory(baseDownload);
+            }
+
+            //inventory
+            string[] replaceFiles = {
+                @"HelloTime.sln",
+                @"HelloTime\App_Start\RouteConfig.cs",
+                @"HelloTime\Global.asax.cs",
+                @"HelloTime\Global.asax",
+                @"HelloTime\Properties\PublishProfiles\WD.pubxml",
+                @"HelloTime\App_Start\WebApiConfig.cs",
+                @"HelloTime\Controllers\HelloController.cs",
+                @"HelloTime\HelloTime.csproj",
+                @"HelloTime\Properties\AssemblyInfo.cs",
+                @"HelloTime\readme.aspx.cs",
+                @"HelloTime\readme.aspx",
+                @"HelloTime\readme.aspx.designer.cs",
+                @"HelloTime\App_Start\FilterConfig.cs" };
+
+            foreach (string r in replaceFiles)
+            {
+                string file = baseFolder + r;
+                string txt = File.ReadAllText(file);
+                txt = txt.Replace("HelloTime", WebAPIName);
+                if (file.Contains("AssemblyInfo"))
+                {
+                    txt = txt.Replace("bf297e58-ada2-483c-bea4-9afe8a8e8c61", assemblyGuid.ToString());
+                }
+                File.WriteAllText(file, txt);
+            }
+
+            File.Move(baseFolder + "HelloTime.sln", baseFolder + WebAPIName + ".sln");
+            File.Move(baseFolder + "HelloTime.v12.suo", baseFolder + WebAPIName + ".v12.suo");
+            File.Move(baseFolder + "HelloTime\\HelloTime.csproj", baseFolder + "HelloTime\\" + WebAPIName + ".csproj");
+            File.Move(baseFolder + "HelloTime\\HelloTime.csproj.user", baseFolder + "HelloTime\\" + WebAPIName + ".csproj.user");
+
+            Directory.Move(baseFolder + "HelloTime", (baseFolder + WebAPIName));
+            string downloadZip = baseDownload + WebAPIName + ".zip";
+
+            using (FileStream stream = new FileStream(downloadZip, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(stream, ZipArchiveMode.Create))
+                {
+                    foreach (string path in Directory.GetFiles(baseFolder, "*.*", SearchOption.AllDirectories))
+                    {
+                        string dir = Path.GetDirectoryName(path);
+                        string newdir = dir.Replace(baseFolder, "");
+                        if (newdir.Substring(1, 1) != ":")
+                        {
+                            Console.WriteLine(newdir);
+                            archive.CreateEntry(newdir + "\\", CompressionLevel.Fastest);
+                        }
+                    }
+
+                    foreach (string path in Directory.GetFiles(baseFolder, "*.*", SearchOption.AllDirectories))
+                    {
+                        byte[] bytes = File.ReadAllBytes(path);
+                        ZipArchiveEntry entry = archive.CreateEntry(path.Replace(baseFolder, ""), CompressionLevel.Fastest);
+                        using (Stream content = entry.Open())
+                        {
+                            content.Write(bytes, 0, bytes.Length);
+                        }
+                    }
+                }
+            }
+
+            //download to HTTP client
+            HttpContext.Current.Response.ContentType = "application/zip";
+            HttpContext.Current.Response.AppendHeader("Content-Disposition", "attachment; filename=" + WebAPIName + ".zip");
+            HttpContext.Current.Response.TransmitFile(downloadZip);
+            HttpContext.Current.Response.End();
+
+            //clean up
+            if (Directory.Exists(baseFolder))
+            {
+                Directory.Delete(baseFolder, true);
+            }
+            string zipfile = WebAPIName + ".zip";
+            if (File.Exists(zipfile))
+            {
+                File.Delete(zipfile);
+            }
+
+        }
+    }
+}
